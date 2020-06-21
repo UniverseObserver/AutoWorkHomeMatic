251a55b593c13ce9dca7acefad5397cd8d662a1e
AOIDUO <zhouqi008012@gmail.com>
2020-06-22 00:01:19 +0800

    automatic grader

--- automatic_grader.py
+++ automatic_grader.py
@@ -18,7 +18,8 @@ submissions_dir = "./res/img/example1_submissions"
 #%%
 # Step 1, 2
 
-def get_images(blank_dir, answer_dir, submissions_dir, display_blank_img=False, display_answer_img=False, display_submis_img=False, print_submis_name=False):
+def get_images(blank_dir, answer_dir, submissions_dir, display_blank_img=False, \
+    display_answer_img=False, display_submis_img=False, print_submis_name=False):
     blank_img = read_img(blank_dir)
     answer_img = read_img(answer_dir)
 
@@ -43,7 +44,8 @@ def get_images(blank_dir, answer_dir, submissions_dir, display_blank_img=False, 
     return blank_img, answer_img, sumbissions_img
 
 
-def run_yolo(blank_img, answer_img, submissions_img, display_blank_img=False, display_answer_img=False, display_submis_img=False):
+def run_yolo(blank_img, answer_img, submissions_img, display_blank_img=False, \
+    display_answer_img=False, display_submis_img=False):
     blank_img_CNN = img_CNNmode(blank_img)
     blank_text_boxes = predict_func(blank_img_CNN,intersec_over_union=0.12, display_img=display_blank_img )
 
@@ -108,23 +110,31 @@ def judge(answer_texts, submissions_map, submissions_texts):
 
 
 #%%
-blank_img, answer_img, sumbissions_img = get_images(blank_dir, answer_dir, submissions_dir, display_blank_img=False, display_answer_img=False, display_submis_img=False, print_submis_name=True)
-blank_text_boxes, answer_text_boxes, submissions_text_boxes = run_yolo(blank_img, answer_img, sumbissions_img, display_blank_img=True, display_answer_img=True, display_submis_img=False)
+def grade(blank_dir, answer_dir, submissions_dir):
+
+    blank_img, answer_img, sumbissions_img = \
+        get_images(blank_dir, answer_dir, submissions_dir, display_blank_img=False,\
+        display_answer_img=False, display_submis_img=False, print_submis_name=True)
+
+    blank_text_boxes, answer_text_boxes, submissions_text_boxes = \
+        run_yolo(blank_img, answer_img, sumbissions_img, \
+        display_blank_img=True, display_answer_img=True, display_submis_img=False)
+
+    submis_map = [ map_submission_to_answer(answer_text_boxes, submis_text_boxes) \
+        for submis_text_boxes in submissions_text_boxes]
+    # print(submis_map)
+
+    answer_texts = get_text_from_boxes(answer_text_boxes, answer_img)
+    submissions_texts = [ get_text_from_boxes(text_box, img) \
+        for (text_box, img) in zip (submissions_text_boxes, sumbissions_img) ]
+
+    result = judge(answer_texts, submis_map, submissions_texts)
+
+    return blank_img, answer_img, sumbissions_img, \
+        blank_text_boxes, answer_text_boxes, submissions_text_boxes, \
+        answer_texts, submis_map, submissions_texts, result    
+
 
 #%%
-submis_map = [ map_submission_to_answer(answer_text_boxes, submis_text_boxes) for submis_text_boxes in submissions_text_boxes]
-# print(submis_map)
-
-#%%
-answer_texts = get_text_from_boxes(answer_text_boxes, answer_img)
-submissions_texts = [ get_text_from_boxes(text_box, img) for (text_box, img) in zip (submissions_text_boxes, sumbissions_img) ]
-
-# print("Answer:")
-# print(answer_texts)
-# print("")
-# print("Submission:")
-# print(submissions_texts)
-
-
-x = judge(answer_texts, submis_map, submissions_texts)
-x[0]    
+if __name__ == '__main__':
+    grade(blank_dir, answer_dir, submissions_dir)
--- dev.py
+++ dev.py
@@ -97,6 +97,56 @@ print( pytesseract.image_to_string(sss) )
 
 
 
+
+
+
+
+
+
+
+###################################################33
+# 在这之前的代码似乎没用了
+
+
+
+
+
+# %%
+def print_a_answer(grade_result, submission_index, question_index ):
+    # grade_result is the output directly derived from the function grade()
+
+    _, answer_img, sumbissions_img, \
+    _, answer_text_boxes, submissions_text_boxes, \
+    answer_texts, submis_map, submissions_texts, result    = grade_result
+
+    print("Question {} from student #{} is ".format(question_index,submission_index ) + \
+    ("CORRECT" if (result[submission_index][question_index]) else "WRONG") )
+
+    print("ground-true answer: " + answer_texts[question_index])
+    boxes = answer_text_boxes[question_index]
+    img = answer_img[ int(boxes[1]):int(boxes[3]), int(boxes[0]):int(boxes[2]) ]
+    plt.imshow(img)
+    plt.show()
+
+    print("student's answer: " + submissions_texts[0][submis_map[submission_index][question_index]])
+    boxes = submissions_text_boxes[submission_index][submis_map[submission_index][question_index]]
+    img = sumbissions_img[0][ int(boxes[1]):int(boxes[3]), int(boxes[0]):int(boxes[2]) ]
+    plt.imshow(img)
+    plt.show()
+
+# %%
+from main import *
+x = grade(blank_dir, answer_dir, submissions_dir)
+print_a_answer(x,1,3)
+
+
+
+
+
+
+
+# %%
+
 # %%
 
 # %%
